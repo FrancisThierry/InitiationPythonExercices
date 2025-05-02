c3da14ff6a2539b842d8836e3d7d65b213d6cb4e
MelvDouc <melv.douc@gmail.com>
2025-05-02 14:39:59 +0200

    Completed exercises

---
+++ Exercices/Car/exos_faits__MelvinD/car.py
@@ -0,0 +1,68 @@
+# Exercice à partir du fichier car.txt
+# Écrire par marque, générer un fichier contenant la marque et le prix remisé pour les modèles du second trimestre
+
+from csv import DictReader, DictWriter
+from datetime import date
+from os.path import dirname, realpath, join as pathJoin
+
+
+class Car():
+    def __init__(self, make: str, ecoBonus: float, price: float, creationDate: date):
+        self.make = make
+        self.ecoBonus = ecoBonus
+        self.price = price
+        self.creationDate = creationDate
+
+    def is2ndTrimester(self) -> bool:
+        month = self.creationDate.month
+        return month >= 4 and month <= 6
+
+    def getReducedPrice(self) -> float:
+        return self.price * (1.0 - self.ecoBonus / 100.0)
+
+
+def getCars(fileName: str) -> list[Car]:
+    cars = []  # type: list[Car]
+
+    with open(fileName) as csvFile:
+        reader = DictReader(csvFile)
+
+        for row in reader:
+            car = Car(
+                make=row["marque"],
+                ecoBonus=float(row["bonus"]),
+                price=float(row["prixHT"]),
+                creationDate=date.fromisoformat(row["dateModele"])
+            )
+            cars.append(car)
+
+    return cars
+
+
+def writeCars(cars: list[Car], outFileName: str) -> None:
+    with open(outFileName, "w") as csvFile:
+        writer = DictWriter(csvFile, fieldnames=["make", "reducedPrice"])
+        writer.writeheader()
+
+        for car in cars:
+            if car.is2ndTrimester():
+                make = car.make
+                reducedPrice = car.getReducedPrice()
+                writer.writerow({"make": make, "reducedPrice": reducedPrice})
+
+
+def main() -> None:
+    scriptDir = dirname(realpath(__file__))
+    srcFileName = pathJoin(scriptDir, "..", "carInput.txt")
+    outFileName = pathJoin(scriptDir, "output.txt")
+
+    try:
+        cars = getCars(srcFileName)
+        writeCars(cars, outFileName)
+        print("Done.")
+    except:
+        print("Something went wrong while writing the data. Try again tomorrow.")
+
+
+if __name__ == "__main__":
+    main()
